24d5ee0774a160518d7b39990683a42c4110472e
ProfO2604 <ProfO2604@lab.aca>
2024-09-23 21:56:37 -0500

    entrada de datos completada

--- src/components/EntradaDatos.jsx
+++ src/components/EntradaDatos.jsx
@@ -1,11 +1,17 @@
 const EntradaDatos = (props) => {
+    const valueOnChange = (evt) => {
+        props.setValor(evt.target.value)
+    }
+
     return <div class="mb-3">
         <label htmlFor="txt_value" className="form-label">
             { props.label }
         </label>
         <input id="txt_value" 
             type={ props.tipo === "entrada" ? "text" : "password" } 
-            className="form-control"/>
+            className="form-control"
+            onChange={ valueOnChange }
+            value={ props.valor }/>
     </div>
 }
 
--- src/components/LoginFormulario.jsx
+++ src/components/LoginFormulario.jsx
@@ -1,13 +1,34 @@
+import { useState } from "react"
 import EntradaDatos from "./EntradaDatos"
 
 const LoginFormulario = () => {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
     return <>
         <h1>Login</h1> 
         <form>
-            <EntradaDatos label="Usuario:" tipo="entrada"/>
-            <EntradaDatos label="Password:" tipo="password"/>
+            <EntradaDatos 
+                label="Usuario:" 
+                tipo="entrada"
+                valor={ username }
+                setValor={ setUsername }/>
+            <EntradaDatos 
+                label="Password:" 
+                tipo="password"
+                valor={ password }
+                setValor={ setPassword }/>
             <div>
-                <button type="button" className="btn btn-success">Login</button>
+                <button type="button" className="btn btn-success"
+                    onClick={ () => {
+                        if (username === "pw" && password === "2024") {
+                            console.log("LOGIN CORRECTO!")
+                        }else {
+                            console.log("ERROR LOGIN")
+                        }
+                    } }>
+                    Login
+                </button>
             </div>
         </form>
     </>

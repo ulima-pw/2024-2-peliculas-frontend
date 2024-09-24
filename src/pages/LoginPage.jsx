2be7f75704f096068e58b442ac30ad2ca2866f20
ProfO2604 <ProfO2604@lab.aca>
2024-09-23 20:34:23 -0500

    agregando bootstrap

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,15 @@
+import "../../node_modules/bootstrap/dist/css/bootstrap.css"
+
 import LoginFormulario from "../components/LoginFormulario"
 
 const LoginPage = () => {
-    return <LoginFormulario />
+    return <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4">
+            <LoginFormulario />
+        </div>
+        <div className="col-md-4"></div>
+    </div> 
 }
 
 export default LoginPage

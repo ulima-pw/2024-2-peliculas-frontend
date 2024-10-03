3f664bab2472d63cc24059775889fc1b329f008b
ProfO2304 <ProfO2304@lab.aca>
2024-10-02 22:00:12 -0500

    prueba peticion http

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,8 +4,46 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import LoginFormulario from "../components/LoginFormulario"
 import { useState } from "react"
 
+const httpObtenerPeliculas = async () => {
+    const url = "https://script.google.com/macros/s/AKfycbx94l1jH8sNlYXGnAV9ImAbzv4mJ13F8lFkYVd61s95dmmwB92Z_uKDpbFH3zFr-KzD/exec?entity=peliculas"
+
+    try {
+        const resp = await fetch(url)
+        const data = await resp.json()
+        console.log(data)
+    }catch(error){
+        console.error(error)
+    }
+
+    /* fetch(url).then((resp) => {
+        resp.json().then((data) => {
+            console.log(data)
+        }).catch((error) => {
+            console.error(error)
+        })
+    }).catch((error) => {
+        console.error(error)
+    }) */
+
+    /* const promesa = fetch(url)
+    promesa.then((resp) => {
+        const promesa2 = resp.json()
+        promesa2.then((data) => {
+            console.log(data)
+        })
+        promesa2.catch((error) => {
+            console.error(error)
+        })
+    })
+    promesa.catch((error) => {
+        console.error(error)
+    }) */
+}
+
 const LoginPage = () => {
     const [error, setError] = useState("")
+
+    httpObtenerPeliculas();
 
     const navigate = useNavigate()
 

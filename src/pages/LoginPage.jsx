b092bf6ae75793de91dfc663ea42e7d2eafda9d7
ProfO2304 <ProfO2304@lab.aca>
2024-10-16 21:50:16 -0500

    conexion login con backend

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,60 +4,37 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css"
 import LoginFormulario from "../components/LoginFormulario"
 import { useState } from "react"
 
-const httpObtenerPeliculas = async () => {
-    const url = "https://script.google.com/macros/s/AKfycbx94l1jH8sNlYXGnAV9ImAbzv4mJ13F8lFkYVd61s95dmmwB92Z_uKDpbFH3zFr-KzD/exec?entity=peliculas"
-
-    try {
-        const resp = await fetch(url)
-        const data = await resp.json()
-        console.log(data)
-    }catch(error){
-        console.error(error)
-    }
-
-    /* fetch(url).then((resp) => {
-        resp.json().then((data) => {
-            console.log(data)
-        }).catch((error) => {
-            console.error(error)
-        })
-    }).catch((error) => {
-        console.error(error)
-    }) */
-
-    /* const promesa = fetch(url)
-    promesa.then((resp) => {
-        const promesa2 = resp.json()
-        promesa2.then((data) => {
-            console.log(data)
-        })
-        promesa2.catch((error) => {
-            console.error(error)
-        })
-    })
-    promesa.catch((error) => {
-        console.error(error)
-    }) */
-}
 
 const LoginPage = () => {
     const [error, setError] = useState("")
 
-    httpObtenerPeliculas();
-
     const navigate = useNavigate()
 
-    const loginHandler = (username, password, paisId) => {
-
-        const usuarioToLogin = JSON.parse(sessionStorage.getItem("USUARIO"))
-
-        if (username === usuarioToLogin.usuario && password === usuarioToLogin.password) {
-            console.log("Pais:" + paisId )
-            navigate("/main")
-        }else {
-            console.log("ERROR LOGIN")
-            setError("ERROR LOGIN")
+    const loginHandler = async (username, password, paisId) => {
+        const data = {
+            usuario : username,
+            password : password
         }
+        const resp = await fetch("http://localhost:3000/login", {
+            method : "POST",
+            body : JSON.stringify(data),
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        })
+        const dataResp = await resp.json()
+
+        if (dataResp.error === "")
+        {
+            // Login correcto
+            setError("")
+            navigate("/main")
+        }else
+        {
+            // Login incorrecto
+            setError(dataResp.error)
+        }
+        
     }
 
     /*const showMessage = (error) => {

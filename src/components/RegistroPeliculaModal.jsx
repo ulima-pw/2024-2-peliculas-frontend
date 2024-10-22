979bb3cb41a44d406a8368e5b6934fcda044a5c8
ProfO2604 <ProfO2604@lab.aca>
2024-10-21 21:27:40 -0500

    Implementando componente Modal

---
+++ src/components/RegistroPeliculaModal.jsx
@@ -0,0 +1,21 @@
+const RegistroPeliculaModal = () => {
+    return <div className="modal fade" id="exampleModal" tabindex="-1">
+        <div className="modal-dialog">
+            <div className="modal-content">
+                <div className="modal-header">
+                    <h1 className="modal-title fs-5" >Modal title</h1>
+                    <button type="button" className="btn-close"></button>
+                </div>
+                <div className="modal-body">
+                    Esto es una prueba
+                </div>
+                <div className="modal-footer">
+                    <button type="button" className="btn btn-secondary">Cerrar</button>
+                    <button type="button" classNameName="btn btn-primary">Guardar changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default RegistroPeliculaModal
